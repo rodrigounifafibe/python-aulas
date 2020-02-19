ca53458acf6c3993048aaec5dbca448c510aa26a
rodrigorgtic <rodrigo.rgtic@gmail.com>
2020-02-19 09:34:41 -0300

    Codigos das Aulas

---
+++ 002 - #TASKS - Tipos primitivos de dados e operações Aritméticas/003_TASK.py
@@ -0,0 +1,11 @@
+""" 
+  > TASK 3:
+  Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada.
+"""
+
+n = int(input('Digite um número: '))
+dobro = n * 2
+triplo = n * 3
+raizQuadrada = n ** (1/2)
+print('O dobro de {} vale {}.'.format(n, dobro))
+print('O triplo de {} vale {}.'.format(n, triplo))
---
+++ 002 - #TASKS - Tipos primitivos de dados e operações Aritméticas/006_TASK.py
@@ -0,0 +1,18 @@
+""" 
+  > TASK 6:
+  Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+"""
+
+numero = int(input('Digite um número para ver a tabuada: '))
+
+# dois caracteres para manter os numeros alinhados.
+print('{} x {:2} = {}'.format(numero, 1, numero * 1))
+print('{} x {:2} = {}'.format(numero, 2, numero * 2))
+print('{} x {:2} = {}'.format(numero, 3, numero * 3))
+print('{} x {:2} = {}'.format(numero, 4, numero * 4))
+print('{} x {:2} = {}'.format(numero, 5, numero * 5))
+print('{} x {:2} = {}'.format(numero, 6, numero * 6))
+print('{} x {:2} = {}'.format(numero, 7, numero * 7))
+print('{} x {:2} = {}'.format(numero, 8, numero * 8))
+print('{} x {:2} = {}'.format(numero, 9, numero * 9))
+print('{} x {:2} = {}'.format(numero, 10, numero * 10))
